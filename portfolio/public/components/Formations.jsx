e4e247539fbeffa4a139bdcecc8364f0edd4d025
Kauan Constancio <kauanconstancio13@gmail.com>
2025-08-29 16:49:05 -0300

    fixing line of skills

--- portfolio/public/components/Formations.jsx
+++ portfolio/public/components/Formations.jsx
@@ -5,21 +5,26 @@ const Formations = () => {
         <span className="text-neon-green font-extrabold text-2xl">+</span>{" "}
         Formação
       </h2>
-      <div className="mt-8 mr-5 flex flex-col gap-3 bg-grayBg p-3 border-1 border-border-white rounded-border-standard text-primary-white">
-        <div>
-          <p className=" font-bold">Análise e Desenvolvimento de Sistemas</p>
-          <div className="flex justify-between items-center">
-            <p className="text-neon-green">Estácio</p>
-            <p className="text-neon-green bg-dark-green pl-3 pr-3 pt-2 pb-2 rounded-full">
-              Em andamento
+      <div>
+        <div className="mt-8 mr-5 flex flex-col gap-3 bg-grayBg p-3 border-1 border-border-white rounded-border-standard text-primary-white">
+          <div>
+            <p className="font-bold text-xl">
+              Análise e Desenvolvimento de Sistemas
             </p>
+            <div className="flex justify-between items-center mt-2">
+              <p className="text-neon-green">Estácio</p>
+              <p className="text-neon-green bg-dark-green pl-3 pr-3 pt-2 pb-2 rounded-full">
+                Em andamento
+              </p>
+            </div>
           </div>
+          <p className="">
+            Cursando Análise e Desenvolvimento de Sistemas, desenvolvendo minhas
+            habilidades em desenvolvimento de software, arquitetura de sistemas
+            e banco de dados.
+          </p>
         </div>
-        <p className="">
-          Cursando Análise e Desenvolvimento de Sistemas, desenvolvendo minhas
-          habilidades em desenvolvimento de software, arquitetura de sistemas e
-          banco de dados.
-        </p>
+        {/* Adicionar Formações apartir daqui */}
       </div>
     </div>
   );
--- portfolio/public/components/Skills.jsx
+++ portfolio/public/components/Skills.jsx
@@ -9,7 +9,10 @@ const Skills = () => {
         <div>
           <Formations />
         </div>
-        
+        <div className=" flex flex-col items-center">
+          <div className="w-4 h-4 bg-neon-green rounded-full"></div>
+          <div className="w-0.5 h-full border-1 border-neon-green border-dashed"></div>
+        </div>
         <div>
           <Experiences />
         </div>
