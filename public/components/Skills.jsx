aab2be4593ab159e2600234211daa4738497aff4
Kauan Constancio <kauanconstancio13@gmail.com>
2025-08-29 17:01:48 -0300

    fixing folders route

--- public/components/Skills.jsx
+++ public/components/Skills.jsx
@@ -5,17 +5,13 @@ const Skills = () => {
   return (
     <div className="mt-15">
       <h1 className="font-bold text-3xl text-neon-green mb-6">Competências</h1>
-      <div className="flex">
-        <div>
-          <Formations />
-        </div>
+      <div className="flex auto-cols-auto">
+        <Formations />
         <div className=" flex flex-col items-center">
           <div className="w-4 h-4 bg-neon-green rounded-full"></div>
           <div className="w-0.5 h-full border-1 border-neon-green border-dashed"></div>
         </div>
-        <div>
-          <Experiences />
-        </div>
+        <Experiences />
       </div>
     </div>
   );
