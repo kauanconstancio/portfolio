b265775d72feaf2ffd7fbd1d1d0c6dc4b8402e1c
Kauan Constancio <kauanconstancio13@gmail.com>
2025-09-09 01:13:26 -0300

    chando w/h tech logos

--- public/components/Tecnologies.jsx
+++ public/components/Tecnologies.jsx
@@ -9,13 +9,13 @@ const Tecnologies = () => {
         <ul className="flex flex-wrap gap-3 justify-center p-8">
           {Tech.map((Techs) => (
             <li
-              className="flex flex-col items-center justify-center gap-3 w-25 h-25 md:w-35 md:h-35 border-border-white border-solid border-2 p-7 rounded-2xl hover:-translate-y-3 hover:border-neon-green transition-all hover:shadow-lg hover:shadow-green-500 ease-in-out"
+              className="flex flex-col items-center justify-center gap-3 w-30 h-30 md:w-35 md:h-35 border-border-white border-solid border-2 p-7 rounded-2xl hover:-translate-y-3 hover:border-neon-green transition-all hover:shadow-lg hover:shadow-green-500 ease-in-out"
               key={Techs.id}
             >
               <img
                 src={Techs.logo}
                 alt={Techs.title}
-                className="md:w-20 w-10"
+                className="md:w-20 w-15"
               />
               <p className="text-white font-medium">{Techs.title}</p>
             </li>
